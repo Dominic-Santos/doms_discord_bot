90772dcaa38ac90a2a48242ff6a70b36cdf3ab3e
Dominic-Santos <68637828+Dominic-Santos@users.noreply.github.com>
2025-08-02 13:20:23 +0100

    half of deck validation tests done

--- tests/src/test_core.py
+++ tests/src/test_core.py
@@ -21,3 +21,86 @@ def test_load_card_database():
     assert len(all_pokemon) == 323
     assert len(trainers.keys()) == 14
     assert len(energies.keys()) == 2
+
+def test_validate_decklist_60_cards():
+    decklist = {}
+    valid, error = validate_decklist(decklist)
+    assert valid is False
+    assert error == "Decklist must contain exactly 60 cards."
+
+def test_validate_decklist_at_least_1_pokemon():
+    decklist = {
+        "energies": {
+            "fire energy": {"quantity": 60}
+        }
+    }
+    valid, error = validate_decklist(decklist)
+    assert valid is False
+    assert error == "Decklist must contain at least 1 Pokémon."
+
+def test_validate_decklist_max_4_copies():
+    decklist = {
+        "pokemon": [
+            {"name": "pikachu", "quantity": 1}
+        ],
+        "energies": {
+            "fire energy": {"quantity": 53}
+        },
+        "trainers": {
+            "boss orders": {"quantity": 6}
+        }
+    }
+    valid, error = validate_decklist(decklist)
+    assert valid is False
+    assert error == "Card boss orders exceeds the maximum of 4 copies."
+
+def test_validate_decklist_one_ace_spec():
+    decklist = {
+        "pokemon": [
+            {"name": "pikachu", "quantity": 1}
+        ],
+        "energies": {
+            "fire energy": {"quantity": 53}
+        },
+        "trainers": {
+            "boss orders": {"quantity": 4},
+            "Ignition Energy": {"quantity": 2}
+        }
+    }
+
+    pokemon, trainers, energies = load_card_database(filename="tests/src/test_legal_cards.json")
+
+    legalcards = {
+        "pokemon": pokemon,
+        "trainers": trainers,
+        "energies": energies
+    }
+    valid, error = validate_decklist(decklist, legal_cards=legalcards)
+    assert valid is False
+    assert error == "Decklist can only contain one Ace Spec card."
+
+
+def test_validate_decklist_illegal_card():
+    decklist = {
+        "pokemon": [
+            {"name": "pikachu", "quantity": 1, "set": "blah", "number": 1}
+        ],
+        "energies": {
+            "fire energy": {"quantity": 54}
+        },
+        "trainers": {
+            "boss orders": {"quantity": 4},
+            "Ignition Energy": {"quantity": 1}
+        }
+    }
+
+    pokemon, trainers, energies = load_card_database(filename="tests/src/test_legal_cards.json")
+
+    legalcards = {
+        "pokemon": pokemon,
+        "trainers": trainers,
+        "energies": energies
+    }
+    valid, error = validate_decklist(decklist, legal_cards=legalcards)
+    assert valid is False
+    assert error == "Card pikachu from set blah is not legal."
