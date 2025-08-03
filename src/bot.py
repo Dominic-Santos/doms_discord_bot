23af187040add7beefc6a01ab678fa4ad5b850ea
Dominic-Santos <68637828+Dominic-Santos@users.noreply.github.com>
2025-08-03 01:31:34 +0100

    base bot all tested or ignored where necessary

--- src/bot.py
+++ src/bot.py
@@ -28,7 +28,7 @@ class Bot(DecklistBot, LegalCardsBot, NewsfeedBot):
 
         @decklist.command(description="Check your decklist is standard legal")
         async def check(ctx, limitless_url: discord.Option(str, "Limitless URL of the decklist")):
-            await self.decklist_check(ctx, limitless_url)
+            await self.decklist_check(ctx, limitless_url)  # pragma: no cover
 
         @tournament.command(description="Sign up for a tournament")
         async def signup(
@@ -38,40 +38,40 @@ class Bot(DecklistBot, LegalCardsBot, NewsfeedBot):
             year_of_birth: discord.Option(int, "Year of birth of the player"),
             limitless_url: discord.Option(str, "Limitless URL of the decklist"),
         ):
-            await self.tournament_signup(ctx, name, pokemon_id, year_of_birth, limitless_url)
+            await self.tournament_signup(ctx, name, pokemon_id, year_of_birth, limitless_url)  # pragma: no cover
 
         @admin.command(description="Update the sign-up sheet")
         async def update_signup_sheet(ctx):
-            await self.update_signup_sheet(ctx)
+            await self.update_signup_sheet(ctx)  # pragma: no cover
 
         @admin.command(description="Sync the legal cards list for deck validation")
         async def update_legal_cards(ctx):
-            await self.get_legal_cards(ctx)
+            await self.get_legal_cards(ctx)  # pragma: no cover
 
         @admin.command(description="Set the output channel for tournament sign-ups")
         async def set_output_channel(ctx):
-            await self.set_output_channel(ctx)
+            await self.set_output_channel(ctx)  # pragma: no cover
 
         @admin.command(description="Test output channel for tournament sign-ups")
         async def test_output_channel(ctx):
-            await self.test_output_channel(ctx)
+            await self.test_output_channel(ctx)  # pragma: no cover
 
         @newsfeed.command(description="Set the newsfeed channel")
         async def set_channel(ctx):
-            await self.set_newsfeed_channel(ctx)
+            await self.set_newsfeed_channel(ctx)  # pragma: no cover
 
         @newsfeed.command(description="Check for newsfeed updates")
         async def update(ctx):
-            await self.get_newsfeed(ctx)
+            await self.get_newsfeed(ctx)  # pragma: no cover
 
         @self.bot.command(description="Get information about the bot")
         async def about(ctx):
-            await ctx.respond("This is a Discord bot built for managing Pokémon TCG events and News.\n\nIt can validate decklists, manage tournament sign-ups, and provide news updates from PokéBeach.\n\nCreated by Dominic Santos (dominatordom8125 on Discord) ", ephemeral=True)
+            await ctx.respond("This is a Discord bot built for managing Pokémon TCG events and News.\n\nIt can validate decklists, manage tournament sign-ups, and provide news updates from PokéBeach.\n\nCreated by Dominic Santos (dominatordom8125 on Discord) ", ephemeral=True)  # pragma: no cover
 
         @self.bot.listen(once=True)
         async def on_ready():
-            self.logger.info(f"Bot is ready! Logged in as {self.bot.user.name} ({self.bot.user.id})")
-            self.add_tasks()
+            self.logger.info(f"Bot is ready! Logged in as {self.bot.user.name} ({self.bot.user.id})")  # pragma: no cover
+            self.add_tasks()  # pragma: no cover
 
     def add_tasks(self):
         time_update_legal_cards = datetime.time(hour=7)
@@ -80,15 +80,15 @@ class Bot(DecklistBot, LegalCardsBot, NewsfeedBot):
 
         @tasks.loop(time=time_update_legal_cards)
         async def update_legal_cards():
-            self.get_legal_cards_task()
+            self.get_legal_cards_task()  # pragma: no cover
 
         @tasks.loop(time=time_update_signup_sheet)
         async def update_signup_sheet():
-            self.update_signup_sheet_task()
+            self.update_signup_sheet_task()  # pragma: no cover
 
         @tasks.loop(**interval_update_newsfeed)
         async def update_newsfeed():
-            await self.get_newsfeed_task()
+            await self.get_newsfeed_task()  # pragma: no cover
 
         update_legal_cards.start()
         update_signup_sheet.start()
--- tests/src/test_bot.py
+++ tests/src/test_bot.py
@@ -1,1 +1,37 @@
 from unittest.mock import patch, MagicMock
+from src.bot import Bot
+
+@patch("src.bot.tasks")
+@patch("src.bot.create_logger")
+@patch("src.bot.discord")
+@patch("src.bot_newsfeed.json")
+@patch("src.bot_decklist.json")
+@patch("builtins.open")
+@patch("src.bot_legalcards.load_card_database")
+def test_bot_init(
+    mock_load,
+    mock_open,
+    mock_dl_json,
+    mock_nf_json,
+    mock_discord,
+    mock_logger,
+    mock_tasks
+):
+    mock_load.return_value = ({}, {}, {})
+    mock_bot = MagicMock()
+    mock_discord.Bot.return_value = mock_bot
+
+    b = Bot("faketoken")
+
+    mock_dl_json.load.assert_called_once()
+    mock_nf_json.load.assert_called_once()
+    
+    mock_discord.Bot.assert_called_once()
+    assert mock_open.call_count == 2
+    mock_logger.assert_called_once()
+
+    b.add_tasks()
+    b.run()
+
+    mock_bot.run.assert_called_once()
+
