f3f3044fba56b8ef797200cfb77d979cbe419f55
Dominic-Santos <68637828+Dominic-Santos@users.noreply.github.com>
2025-08-15 13:44:26 +0100

    better error handling for newsfeed

--- src/pokebeach.py
+++ src/pokebeach.py
@@ -8,14 +8,19 @@ def get_newsfeed() -> list[str]:
     sb.sleep(1)
     post_div = sb.cdp.find_visible_elements("div.xpress_articleList")[0]
     for post in post_div.children:
-        try:
-            a_element = post
-            for _ in range(6):
-                a_element = a_element.children[0]
-            post_url = a_element["href"]
-            posts.append(post_url)
-        except Exception as e:
-            print(e)
-            pass
+        a_element = post
+        empty_post = False
+        for _ in range(6):
+            if len(a_element.children) == 0:
+                empty_post = True
+                break
+            a_element = a_element.children[0]
+
+        if empty_post:
+            continue
+
+        post_url = a_element["href"]
+        posts.append(post_url)
+
     sb.quit()
     return posts
